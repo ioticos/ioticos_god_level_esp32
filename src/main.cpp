ca7d11da6b9e4723ca56da1f9edf7d5db7681fef
Pablo <somosioticos@gmail.com>
2021-01-22 14:31:01 -0300

    post request

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <PubSubClient.h>
 
 String dId = "121212";
-String webhook_pass = "SA7sxAKDVR";
+String webhook_pass = "SA7sxAKDV";
 String webhook_endpoint = "http://192.168.0.6:3001/api/getdevicecredentials";
 const char* mqtt_server = "192.168.0.6";
 
@@ -22,6 +22,7 @@ const char *wifi_ssid = "GOLD2";
 const char *wifi_password = "Tesla208";
 
 //Functions definitions
+bool get_mqtt_credentials();
 void clear();
 
 void setup() {
@@ -48,10 +49,9 @@ void setup() {
       delay(2000);
       ESP.restart();
     }
-
   }
 
-    Serial.print("  ⤵" + fontReset);
+  Serial.print("  ⤵" + fontReset);
 
   //Printing local ip
   Serial.println(boldGreen + "\n\n         WiFi Connection -> SUCCESS :)" + fontReset);
@@ -60,15 +60,52 @@ void setup() {
   Serial.print(WiFi.localIP());
   Serial.println(fontReset);
 
-  
 
-
-  
+  get_mqtt_credentials();
 
 
 }
 
 void loop() {
+
+}
+
+
+
+bool get_mqtt_credentials(){
+
+  Serial.print(underlinePurple + "\n\n\nGetting MQTT Credentials from WebHook" + fontReset + Purple + "  ⤵");
+  delay(1000);
+
+  String toSend = "dId=" + dId + "&password=" + webhook_pass;
+
+  HTTPClient http;
+  http.begin(webhook_endpoint);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int response_code = http.POST(toSend);
+
+
+  if(response_code < 0 ){
+    Serial.print(boldRed + "\n\n         Error Sending Post Request :( " + fontReset);
+    http.end();
+    return false;
+  }
+
+  if(response_code != 200){
+    Serial.print(boldRed + "\n\n         Error in response :(   e-> "  + fontReset + " " + response_code);
+    http.end();
+    return false;
+  }
+
+  if (response_code == 200){
+    String responseBody = http.getString();
+
+    Serial.print(boldGreen + "\n\n         Mqtt Credentials Obtained Successfully :) " + fontReset);
+    Serial.print("\n\n" + responseBody);
+    delay(2000);
+
+  }
 
 }
 
