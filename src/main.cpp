1171d80adfb5bef0475090934114e2a9f080484b
Pablo <somosioticos@gmail.com>
2021-01-25 14:04:45 -0300

    sending variables to broker firsts steps

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ void check_mqtt_connection();
 bool reconnect();
 void process_sensors();
 void process_actuators();
+void send_data_to_broker();
 void clear();
 
 //Global Vars
@@ -78,19 +79,16 @@ void setup()
 void loop()
 {
   check_mqtt_connection();
+  send_data_to_broker();
 
   process_sensors();
   process_actuators();
 
-  delay(5000);
-  serializeJsonPretty(mqtt_data_doc, Serial);
 }
 
 
 int prev_temp = 0;
 int prev_hum = 0;
-
-
 void process_sensors(){
 
 
@@ -142,6 +140,44 @@ void process_actuators(){
   }
 }
 
+
+//TEMPLATE ⤵
+
+long varsLastSend[20];
+
+void send_data_to_broker(){
+
+  long now = millis();
+
+  for(int i = 0; i < mqtt_data_doc["variables"].size(); i++){
+
+    if (mqtt_data_doc["variables"][i]["variableType"] == "output"){
+      continue;
+    }
+
+    int freq = mqtt_data_doc["variables"][i]["variableSendFreq"];
+
+    if (now - varsLastSend[i] > freq * 1000){
+      varsLastSend[i] = millis();
+
+      String str_root_topic = mqtt_data_doc["topic"];
+      String str_variable = mqtt_data_doc["variables"][i]["variable"];
+      String topic = str_root_topic + str_variable + "/sdata";
+
+      String toSend = "";
+
+      serializeJson(mqtt_data_doc["variables"][i]["last"], toSend);
+
+      client.publish(topic.c_str(), toSend.c_str());
+
+
+    }
+    
+
+
+  }
+
+}
 
 bool reconnect()
 {
